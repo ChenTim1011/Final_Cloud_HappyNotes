a80ef7f5ede994f138a698e34df85e061a277cf6
TimChen1011 <110208059@g.nccu.edu.tw>
2024-12-22 13:03:24 +0800

    feat: update database connection to use environment variables and enable SSL for AWS DocumentDB

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+require("dotenv").config();
 
-// Connect to MongoDB
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_SSL_CA = process.env.DB_SSL_CA;
+
+const sslCA = fs.readFileSync(DB_SSL_CA, "utf8");
+
+const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/whiteboards", {})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Error connecting to MongoDB:", error));
+  .connect(uri, {
+    ssl: true,
+    sslCA: sslCA,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to AWS DocumentDB"))
+  .catch((error) =>
+    console.error("Error connecting to AWS DocumentDB:", error)
+  );
 
 module.exports = mongoose;
