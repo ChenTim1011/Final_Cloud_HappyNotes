51cc7cf3a19b005420c607d714420bfc352f7e9f
TimChen1011 <110208059@g.nccu.edu.tw>
2024-12-22 14:33:59 +0800

    feat: refactor database connection to streamline SSL configuration and use environment variables

--- src/backend/config/db.js
+++ src/backend/config/db.js
@@ -2,16 +2,9 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 require("dotenv").config();
 
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-const DB_SSL_CA = process.env.DB_SSL_CA;
+const sslCA = fs.readFileSync(process.env.DB_SSL_CA, "utf8");
 
-const sslCA = fs.readFileSync(DB_SSL_CA, "utf8");
-
-const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?tls=true&tlsCAFile=${process.env.DB_SSL_CA}&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
 
 mongoose
   .connect(uri, {
