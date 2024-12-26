634b35d1eaf526eace9157c66ce01eb2e6e75b2b
TimChen1011 <110208059@g.nccu.edu.tw>
2024-12-26 20:43:55 +0800

    feat: add shadcn UI components and improve configuration for Tailwind CSS

--- src/frontend/react-frontend/tailwind.config.js
+++ src/frontend/react-frontend/tailwind.config.js
@@ -2,7 +2,8 @@
 // This line provides TypeScript support and ensures proper IntelliSense for the Tailwind CSS configuration
 
 export default {
-  // **Content Paths**:
+    darkMode: ["class"],
+    // **Content Paths**:
   // Specify the files Tailwind should scan to generate styles.
   // This includes the index.html file and all files within the "src" directory
   // with the extensions .js, .ts, .jsx, and .tsx.
@@ -12,11 +13,59 @@ export default {
   // Extend the default Tailwind theme here. This section is used to customize
   // default styles, add new utilities, or override existing ones.
   theme: {
-    extend: {}, // Currently empty but can be extended with custom theme settings
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
 
   // **Plugins**:
   // Add Tailwind CSS plugins here to extend the framework's functionality.
   // This can include custom plugins or official Tailwind plugins for additional features.
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
