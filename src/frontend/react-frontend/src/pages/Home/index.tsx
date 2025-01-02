1e225a888e6fb5561f837efae075b71073585af4
TimChen1011 <110208059@g.nccu.edu.tw>
2025-01-02 12:19:51 +0800

    modify: update YouTube video source in Home component

--- src/frontend/react-frontend/src/pages/Home/index.tsx
+++ src/frontend/react-frontend/src/pages/Home/index.tsx
@@ -59,7 +59,7 @@ const Home: React.FC = () => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                src="https://www.youtube.com/embed/52FKj3UIpSw"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
